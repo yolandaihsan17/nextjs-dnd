1a9e0ba4da3cbca5921623204a5615d6dc5ca119
yolanda <yolanda@landx.id>
2022-12-10 19:33:51 +0700

    dnd done

--- pages/manage-event/index.tsx
+++ pages/manage-event/index.tsx
@@ -1,5 +1,5 @@
 import { Eye, More, Edit2, Video, Clock, ImportCurve, AddSquare, Add, Location } from "iconsax-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Layout from "../../components/layout";
 import { getDate, getDuration } from "../../utils/util";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
@@ -63,134 +63,6 @@ export default function ManagePage() {
 
   const [selectedSession, setSelectedSession] = useState<SessionProps>(session[0])
   const [selectedLesson, setSelectedLesson] = useState<LessonProps>(session[0]?.lessons[0])
-
-  // const [placeholderProps, setPlaceholderProps] = useState({});
-  // const queryAttr = "data-rbd-drag-handle-draggable-id";
-
-  // // // Function to update list on drop
-  // // const handleDrop = (droppedItem: any) => {
-  // //   // Ignore drop outside droppable container
-  // //   if (!droppedItem.destination) return;
-  // //   let updatedList = [...session];
-  // //   // Remove dragged item
-  // //   const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
-  // //   // Add dropped item
-  // //   updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
-  // //   // Update State
-  // //   setSession(updatedList);
-  // // };
-
-
-
-  // const getDraggedDom = (draggableId: any) => {
-  //   const domQuery = `[${queryAttr}='${draggableId}']`;
-  //   const draggedDOM = document.querySelector(domQuery);
-
-  //   return draggedDOM;
-  // };
-
-  // const onDragEnd = (result: any) => {
-  //   const { source, destination } = result;
-
-  //   // if the user drops outside of a droppable destination
-  //   if (!destination) return;
-
-  //   // If the user drags and drops back in the same position
-  //   if (
-  //     destination.droppableId === source.droppableId &&
-  //     destination.index === source.index
-  //   ) {
-  //     return;
-  //   }
-
-  //   // If the user drops in a different postion
-  //   const tasks = session;
-  //   const newTasks = reorderTasks(tasks, source.index, destination.index);
-
-  //   const newState = {
-  //     ...session,
-  //     tasks: newTasks,
-  //   };
-  //   setSession(newState);
-  // };
-
-  // const onDragUpdate = (result: any) => {
-  //   const { source, destination, draggableId } = result;
-
-  //   if (!destination) return;
-
-  //   const draggedDOM = getDraggedDom(draggableId);
-
-  //   if (!draggedDOM?.parentNode) return;
-
-  //   const { clientHeight, clientWidth } = draggedDOM;
-  //   const destinationIndex = destination.index;
-  //   const sourceIndex = source.index;
-
-  //   const childrenArray = draggedDOM.parentNode.children
-  //     ? [...draggedDOM.parentNode.children]
-  //     : [];
-
-  //   const movedItem = childrenArray[sourceIndex];
-  //   childrenArray.splice(sourceIndex, 1);
-
-  //   const updatedArray = [
-  //     ...childrenArray.splice(0, destinationIndex),
-  //     movedItem,
-  //     ...childrenArray.splice(destinationIndex + 1),
-  //   ];
-
-  //   const clientY =
-  //     parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingTop) +
-  //     updatedArray.splice(0, destinationIndex).reduce((total, current) => {
-  //       const style = current.currentStyle || window.getComputedStyle(current);
-  //       const marginBottom = parseFloat(style.marginBottom);
-  //       return total + current.clientHeight + marginBottom;
-  //     }, 0);
-
-  //   setPlaceholderProps({
-  //     clientHeight,
-  //     clientWidth,
-  //     clientY,
-  //   });
-  // };
-
-  // const onDragStart = (result) => {
-  //   const { source, draggableId } = result;
-  //   const draggedDOM = getDraggedDom(draggableId);
-
-  //   if (!draggedDOM) return;
-
-  //   const { clientHeight, clientWidth } = draggedDOM;
-  //   const sourceIndex = source.index;
-
-  //   if (!draggedDOM.parentNode) return;
-
-  //   /**
-  //    * 1. Take all the items in the list as an array
-  //    * 2. Slice from the start to the where we are dropping the dragged item (i.e destinationIndex)
-  //    * 3. Reduce and fetch the styles of each item
-  //    * 4. Add up the margins, widths, paddings
-  //    * 5. Accumulate and assign that to clientY
-  //    */
-  //   const clientY =
-  //     parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingTop) +
-  //     [...Array.from(draggedDOM.parentNode.children)]
-  //       .slice(0, sourceIndex)
-  //       .reduce((total, current) => {
-  //         const style =
-  //           current.currentStyle || window.getComputedStyle(current);
-  //         const marginBottom = parseFloat(style.marginBottom);
-
-  //         return total + current.clientHeight + marginBottom;
-  //       }, 0);
-
-  //   setPlaceholderProps({
-  //     clientHeight,
-  //     clientWidth,
-  //     clientY,
-  //   });
-  // };
 
   const lessonChanged = (data: LessonProps) => {
     // get available session and save it to temporary variable
@@ -286,9 +158,68 @@ export default function ManagePage() {
     console.log(session)
   }, [session])
 
+
+  const dragItem = useRef<any>(0);
+  const dragOverItem = useRef<any>(0);
+
+  const dragStart = (e: any, position: any) => {
+    dragItem.current = position;
+  };
+
+  const dragEnter = (e: any, position: any) => {
+    dragOverItem.current = position;
+  };
+
+
+  const drop = (e: any) => {
+    const copyListItems = [...session];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setSession(copyListItems);
+  };
+
+
+  const dragItemLesson = useRef<any>(0);
+  const dragOverItemLesson = useRef<any>(0);
+  const sessionRef = useRef<any>(0)
+
+  const dragStartLesson = (e: any, position: any, sessionIndex: any) => {
+    dragItemLesson.current = position;
+    sessionRef.current = sessionIndex
+  };
+
+  const dragEnterLesson = (e: any, position: any, sessionIndex: any) => {
+    dragOverItemLesson.current = position;
+    sessionRef.current = sessionIndex
+  };
+
+  const dropLesson = (e: any) => {
+    const sessionTemp = [...session]
+
+    const lessonItems = [...session[sessionRef.current].lessons]
+    const dragLessonContent = lessonItems[dragItemLesson.current]
+
+    lessonItems.splice(dragItemLesson.current, 1);
+    lessonItems.splice(dragOverItemLesson.current, 0, dragLessonContent);
+    
+
+    sessionTemp[sessionRef.current].lessons = lessonItems
+
+    // reset
+    dragItemLesson.current = null;
+    dragOverItemLesson.current = null;
+    sessionRef.current = null
+
+    setSession(sessionTemp)
+  };
+
   return (
     <Layout>
       <div className="flex flex-col items-stretch justify-start gap-2 p-4">
+
         <div className="flex flex-row items-center justify-between mt-8">
           <div className="flex flex-row items-center justify-start gap-8">
             <h1 className="text-4xl font-semibold">Belajar dan praktek cinematic videography</h1>
@@ -304,46 +235,6 @@ export default function ManagePage() {
           <div>Event Schedule: 24 Oktober 2021, 16.30</div>
         </div>
 
-        {/* <DragDropContext onDragEnd={handleDrop}>
-          <Droppable droppableId="list-container">
-            {(provided) => (
-              <div
-                className="list-container"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {session.map((item, index) => (
-                  <Draggable key={'item'} draggableId={item} index={index}>
-                    {(provided) => (
-                      <div className="item-container flex flex-col items-stretch justify-start rounded-md outline outline-1 outline-gray-300 p-8 py-4 mt-8 gap-2" ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-                        <div className="flex flex-row items-center justify-start text-2xl font-bold gap-2">
-                          <DragIcon />
-                          <div>{item.title}</div>
-                          <Edit2 size={20} className='text-gray-400 ml-4' />
-                        </div>
-
-                        <div className="flex flex-col items-stretch justify-start pl-8 mt-4">
-                          {
-                            item.lessons.map((item, index) => (
-                              <Lesson {...item} key={index} />
-                            ))
-                          }
-                        </div>
-
-                        <div className="flex flex-row items-center justify-start gap-4 mt-4">
-                          <AddSquare variant="Bold" className="text-purple-600 cursor-pointer" size={32} />
-                          <div className="font-semibold">Add Lesson Material</div>
-                        </div>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext> */}
-
         {(editMode.isActive && editMode.type === 'lesson') &&
           <EditDialog {...selectedLesson} onChange={(data: LessonProps) => { lessonChanged(data) }}></EditDialog>
         }
@@ -354,9 +245,15 @@ export default function ManagePage() {
 
 
         {session.length > 0 && session.map((session, sessionIndex) => (
-          <div className="item-container flex flex-col items-stretch justify-start rounded-md outline outline-1 outline-gray-300 p-8 py-4 mt-8 gap-2" key={sessionIndex}>
+          <div className="item-container flex flex-col items-stretch justify-start rounded-md outline outline-1 outline-gray-300 p-8 py-4 mt-8 gap-2"
+            key={sessionIndex}
+
+          >
             <div className="flex flex-row items-center justify-start text-2xl font-bold gap-2">
-              <DragIcon />
+              <div draggable
+                onDragStart={(e) => dragStart(e, sessionIndex)}
+                onDragEnter={(e) => dragEnter(e, sessionIndex)}
+                onDragEnd={drop}><DragIcon /></div>
               <div className="ml-2">{session.title}</div>
               <Edit2 size={20} className='text-gray-400 ml-4 cursor-pointer' onClick={() => switchToEditMode(session.id, 'session')} />
             </div>
@@ -364,7 +261,12 @@ export default function ManagePage() {
             <div className="flex flex-col items-stretch justify-start pl-8 mt-4">
               {
                 session.lessons.length > 0 ? session.lessons.map((item, lessonIndex) => (
-                  <Lesson {...item} key={lessonIndex} onEdit={(id: string) => switchToEditMode(session.id, 'lesson', id)} />
+                  <div draggable
+                    onDragStart={(e) => dragStartLesson(e, lessonIndex, sessionIndex)}
+                    onDragEnter={(e) => dragEnterLesson(e, lessonIndex, sessionIndex)}
+                    onDragEnd={dropLesson}>
+                    <Lesson {...item} key={lessonIndex} onEdit={(id: string) => switchToEditMode(session.id, 'lesson', id)} />
+                  </div>
                 )) :
                   <div className="text-center font-bold text-yellow-500">No lesson available</div>
               }
